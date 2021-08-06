500a26a7b268e6f315955220769534908aec7cc4
Camila Dias Figueiredo <camila@MacBook-Pro-de-maxmilhas-2.local>
2021-08-05 23:41:10 -0300

    create main component stories

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,10 +1,40 @@
-import { ReactElement } from 'hoist-non-react-statics/node_modules/@types/react'
 import { Meta, Story } from '@storybook/react'
+import { MainProps } from './interfaces'
 import Main from '.'
 
 export default {
   title: 'Main',
-  component: Main
+  component: Main,
+  argTypes: {
+    title: {
+      name: 'Title',
+      type: { name: 'string', required: true },
+      defaultValue: 'React Avançado',
+      description: 'Titulo da capa',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'React Avançado' }
+      },
+      control: {
+        type: 'text'
+      }
+    },
+    description: {
+      name: 'Description',
+      type: { name: 'string', required: true },
+      defaultValue: 'TypeScript, ReactJS, NextJS e Styled Components',
+      description: 'Subtitulo da capa',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: {
+          summary: 'TypeScript, ReactJS, NextJS e Styled Components'
+        }
+      },
+      control: {
+        type: 'text'
+      }
+    }
+  }
 } as Meta
 
-export const Basic: Story = (): ReactElement => <Main />
+export const Basic: Story<MainProps> = (args) => <Main {...args} />
